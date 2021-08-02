b4ec801a52e871799d6ba4833dd825fecca1a08d
chanti <qakrishna2@gmail.com>
2021-08-02 10:30:14 +0530

    Added new pln method

--- src/com/sample/First.java
+++ src/com/sample/First.java
@@ -3,6 +3,9 @@ package com.sample;
 public class First {
 	public static void main(String[] args) {
 		System.out.println("Added 123 ");
+                System.out.println("Added 456 ");
+
+
 		
 		
 	}
